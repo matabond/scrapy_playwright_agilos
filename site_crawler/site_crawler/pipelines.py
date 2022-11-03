352ab3cd44249ae9b2afc33121e370b9878f269f
Matija Ivanusa <matija.ivanusa@gmail.com>
2022-11-03 14:14:19 +0100

    init verzija rog_joma scrapy koda

--- site_crawler/site_crawler/pipelines.py
+++ site_crawler/site_crawler/pipelines.py
@@ -5,9 +5,48 @@
 
 
 # useful for handling different item types with a single interface
+import pymongo
 from itemadapter import ItemAdapter
 
+from scrapy.exceptions import DropItem
 
-class SiteCrawlerPipeline:
+# class DuplicatesPipeline:
+
+#     def __init__(self):
+#         self.ids_seen = set()
+
+#     def process_item(self, item, spider):
+#         adapter = ItemAdapter(item)
+#         if adapter['id'] in self.ids_seen:
+#             raise DropItem(f"Duplicate item found: {item!r}")
+#         else:
+#             self.ids_seen.add(adapter['id'])
+#             return item
+
+class MongoPipeline:
+
+    collection_name = 'rog_joma'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'rog_joma')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+        # Start with a clean database
+        self.db[self.collection_name].delete_many({})
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert_one(ItemAdapter(item).asdict())
         return item
